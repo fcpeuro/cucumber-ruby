05c040c298950eb80c43b5a6b049a9a8444a3b86
Evan_Esatto <evan.esatto@fcpeuro.com>
2023-07-20 12:30:33 -0400

    fix filename reference

--- lib/cucumber/formatter/junit.rb
+++ lib/cucumber/formatter/junit.rb
@@ -127,7 +127,7 @@ module Cucumber
         duration = ResultBuilder.new(result).test_case_duration
         @current_feature_data[:time] += duration
         classname = @current_feature_data[:feature].name
-        filename = @current_feature_data[:uri]
+        filename = @current_feature_data[:feature].file
         name = scenario_designation
 
         @current_feature_data[:builder].testcase(:classname => classname, :name => name, :time => format('%.6f', duration), :filename => filename) do
